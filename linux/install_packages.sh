d5f3443aabf855f0ad86684910baa7b849a67331
majjoha <mathias@mjj.io>
2013-12-22 21:07:41 +0100

    Update with aptitude as well.

--- linux/install_packages.sh
+++ linux/install_packages.sh
@@ -1,23 +1,14 @@
 # Install the following packages:
-#packages=( 'update' 'safe-upgrade' 'build-essential' 'vim' 'git' 'curl' 'tmux' 'irssi' 'tig' 'zsh' 'haskell-platform' );
-#
+packages=( 'build-essential' 'vim' 'git' 'curl' 'tmux' 'irssi' 'tig' 'zsh' 'haskell-platform' );
 
-packages=( 'update' 'safe-upgrade' 'build-essential' 'vim' 'git' 'curl' 'tmux' 'irssi' 'tig' 'zsh' 'haskell-platform' );
+sudo apt-get -y update
+sudo apt-get -y safe-upgrade
+sudo apt-get -y install aptitude
+sudo aptitude -y update
+sudo aptitude -y safe-upgrade
 
 for package in "${packages[@]}"
 do
-echo -e "\e[0;32mInstalling $package...\e[0m"
+  echo -e "\e[0;32mInstalling $package...\e[0m"
   sudo aptitude install -y "$package"
 done
-
-# Install oh-my-zsh.
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-# Change default shell from bash to zsh.
-chsh -s /bin/zsh
-
-# Install RVM and get the latest stable Ruby release.
-curl -L https://get.rvm.io | bash -s stable --ruby
-
-# Source RVM
-source $HOME/.rvm/scripts/rvm
