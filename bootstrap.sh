131f3965706740b10d0ab5740956530ed9e7e147
majjoha <mathias@mjj.io>
2013-12-22 21:07:29 +0100

    Use bash to install scripts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,24 @@
 
 platform=`uname`
 if [[ "$platform" == 'Linux' ]]; then
-  sh linux/install_packages.sh
+  bash linux/install_packages.sh
 elif [[ "$platform" == 'Darwin' ]]; then
-  sh osx/install_packages.sh
+  bash osx/install_packages.sh
 fi
+
+# Add dotfiles submodule.
+git submodule add https://github.com/majjoha/dotfiles.git
+
+# Install oh-my-zsh.
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# Change default shell from bash to zsh.
+chsh -s /bin/zsh
+
+# Install RVM and get the latest stable Ruby release.
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# Source RVM
+source $HOME/.rvm/scripts/rvm
+
+cd ~/ && ~/machine/bin/symlinktree $HOME/dotfiles
